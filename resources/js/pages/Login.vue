2f9cb278cd29644c299294937f740e97df2bfbba
Anperezna <tete.perez.moreno@gmail.com>
2026-03-13 17:15:00 +0100

    empezar ahor alogin

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -2,11 +2,13 @@
     <div id="container">
         <h1>Login</h1>
         <Input placeholder="Email" />
+        <Botones/>
     </div>
 </template>
 
 <script setup>
 import Input from '@/components/Input.vue';
+import Botones from '@/components/Botones.vue';
 
 </script>
 
