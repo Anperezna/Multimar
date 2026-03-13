232a2fea437ccd508c531d7e7216687bdae89118
danibarrf <daniel.barrufet@gmail.com>
2026-03-13 17:10:40 +0100

    componente boton

--- resources/js/components/Botones.vue
+++ resources/js/components/Botones.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-
-    </div>
+  <button class="btn-login" :disabled="disabled" @click="$emit('click')">
+    <slot>Iniciar sesión</slot>
+  </button>
 </template>
 
 <script setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
 
+defineEmits(['click'])
 </script>
 
 <style lang="scss" scoped>
+.btn-login {
+  padding: 0.5rem 1.25rem;
+  border: none;
+  border-radius: 6px;
+  font-size: 1rem;
+  cursor: pointer;
+  background-color: #0EA5E9;
+  color: #fff;
+  transition: background-color 0.2s;
 
+  &:hover:not(:disabled) {
+    background-color: #0284c7;
+  }
+  
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+}
 </style>
